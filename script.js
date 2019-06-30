7dbe35c40f294d3742e8d4cae4b9806aa7a39e49
Crystal Ophaso <crystalo@procore.com>
2019-06-29 17:04:31 -0700

    fix active css, add active toggle, add api urls

--- script.js
+++ script.js
@@ -2,13 +2,19 @@
 
 "use strict";
 
+const youtubeApiKey = "";
+const zomatoApitKey = "";
+
+const youtubeSearchUrl = "https://www.googleapis.com/youtube/v3/search";
+const zomatoSearchUrl = "https://developers.zomato.com/api/v2.1/search";
+
 var selectionArr = [];
 var inOutFilter = ''
 
-// making div:active stick
 function selectCuisine() {
   $('.cuisineSelection').on('click', '.cuisineOptions', function(event) {
-      selectionArr.push($(event.target).text());
+			selectionArr.push($(event.target).text());
+			$(event.target).toggleClass('active');
       console.log(selectionArr);
   });
 }
@@ -16,15 +22,31 @@ function selectCuisine() {
 function whereToEat() {
   $('.inOrOut').on('click', '.whereToEat', function(event) {
 		inOutFilter = $(event.target).text();
+		$(event.target).toggleClass('active');
 		console.log(inOutFilter);
   });
+}
+
+function determineSearch(inOrOut){
+	if (inOrOut == 'Cook it myself') {
+		//use only the youtube endpoint
+	}
+	else if (inOrOut == 'Show all options') {
+		//use the zomato and youtube endpoint
+	}
+	else if (inOrOut == 'Dine out') {
+		//use only the zomato endpoint
+	};
 }
 
 function watchForm(){
   $('form').submit(event => {
 		event.preventDefault();
+		let zip = $('#zip').val();
+		console.log(zip);
 	})
 }
 
 $(selectCuisine());
 $(whereToEat());
+$(watchForm());
