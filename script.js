6019045c50eb8c3b89765142a3681558ef4d5ed6
Crystal Ophaso <crystalo@procore.com>
2019-07-01 15:19:28 -0700

    get zomato search to work by city

--- script.js
+++ script.js
@@ -1,161 +1,232 @@
 "use strict";
 
+// youtube API key
 const youtubeApiKey = "";
+// zomato API Key
 const zomatoApiKey = "";
 
+
+// youtube search API
 const youtubeSearchUrl = "https://www.googleapis.com/youtube/v3/search";
-const zomatoSearchUrl = "https://developers.zomato.com/api/v2.1/search";
+// zomato API
+const zomatoUrl = "https://developers.zomato.com/api/v2.1";
 
+// Array to store the cuisine selections
 var selectionArr = [];
+// variable to store the dining option
 var inOutFilter = '';
+// hero slideshow
+var slideIndex = 0; 
 
-function getYoutubeVideos(searchTerm, maxResults = 5){
-  for (let i = 0; i < searchTerm.length; i++){
-    const params = {
-      key: youtubeApiKey,
-      q: searchTerm[i] + 'food recipe',
-      part: "snippet",
-      maxResults: maxResults,
-      type: "video"
-    };
-
-    let queryString = $.param(params);
-    console.log("query Stringified", queryString);
-    const url = youtubeSearchUrl + "?" +queryString;
-    console.log("url", url);
-
-    fetch(url).then(response => {
-      if(response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    }).then(responseJson => displayVidResults(responseJson))
-    .catch(err=> {
-      $('#js-error-message').text(`Something Failed ${err.message}`);
-    })
+var cuisineData = [
+  {
+    id: 60,
+    cuisine: 'JAPANESE'
+  },
+  {
+    id: 1,
+    cuisine: 'AMERICAN'
+  },
+  {
+    id: 55,
+    cuisine: 'ITALIAN'
+  },
+  {
+    id: 73,
+    cuisine: 'MEXICAN'
+  },
+  {
+    id: 67,
+    cuisine: 'KOREAN'
+  },
+  {
+    id: 89,
+    cuisine: 'SPANISH'
   }
+]
+
+// HERO SLIDESHOW
+function carousel() {
+  let i;
+  let x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) { slideIndex = 1 }
+  x[slideIndex - 1].style.display = "block";
+  setTimeout(carousel, 2000);
 }
 
-function displayVidResults(responseJson){
-  console.log(responseJson);
+function getYoutubeVideos(searchTerm, maxResults = 5) {
+    for (let i = 0; i < searchTerm.length; i++) {
+        const params = {
+            key: youtubeApiKey,
+            q: searchTerm[i] + 'food recipe',
+            part: "snippet",
+            maxResults: maxResults,
+            type: "video"
+        };
 
-  for (let i=0; i<responseJson.items.length; i++){
-    $('.video-results').append(
-      `<li>
+        let queryString = $.param(params);
+        const url = youtubeSearchUrl + "?" + queryString;
+
+        fetch(url).then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error(response.statusText);
+            }).then(responseJson => displayVidResults(responseJson))
+            .catch(err => {
+                $('#js-error-message').text(`Something Failed ${err.message}`);
+            })
+    }
+}
+
+function displayVidResults(responseJson) {
+    for (let i = 0; i < responseJson.items.length; i++) {
+        $('.video-results').append(
+            `<li>
       <h3>${responseJson.items[i].snippet.title}</h3>
       <p>${responseJson.items[i].snippet.description}</p>
-      <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
+      <a href="https://www.youtube.com/watch?v=${responseJson.items[i].id.videoId}">
+      <img src='${responseJson.items[i].snippet.thumbnails.high.url}'>
+      </a>
       </li>`
-    )
-  };
-  $("#results").removeClass("hidden");
+        )
+    };
+    $("#results").removeClass("hidden");
 }
 
-function getZomatoRest(searchTerm, zip, maxResults = 5){
-  for (let i = 0; i < searchTerm.length; i++){
-    const options = {
-      headers: new Headers({
-        'user-key': zomatoApiKey,
-        'Content-Type': 'application/json'
-      })
-    };
-
-    const params = {
-      entity_id: zip,
-      q: searchTerm[i],
-      count: maxResults
-    };
-
-    let queryString = $.param(params);
-    console.log("query Stringified", queryString);
-    const url = zomatoSearchUrl +  +queryString;
-    console.log("url", url);
-
-    fetch(url, options).then(response => {
-      if(response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    }).then(responseJson => displayRestResults(responseJson))
-    .catch(err=> {
-      $('#js-error-message').text(`Something Failed ${err.message}`);
-    })
+function getCuisineId(searchTerm){
+  for (let i = 0; i < cuisineData.length; i++) {
+    if(searchTerm == cuisineData[i].cuisine){
+      return cuisineData[i].id;
+    }
   }
 }
 
-function displayRestResults(responseJson){
-  console.log(responseJson);
-
-  for (let i=0; i<responseJson.items.length; i++){
-    $('.rest-results').append(
-      `<li>
-      <h3>${responseJson.restaurants[i].name}</h3>
-      <a href='${responseJson.restaurants[i].url}'></a>
-      </li>`
-    )
+function getCityId(searchTerm, city){
+  const options = {
+    headers: new Headers({
+        'user-key': zomatoApiKey
+    })
   };
-  $("#results").removeClass("hidden");
+
+  const params = {
+      q: city
+  };
+
+  let queryString = $.param(params);
+  const url = zomatoUrl + '/cities?' +queryString;
+
+  fetch(url, options).then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error(response.statusText);
+      })
+      .then(responseJson => getZomatoRest(searchTerm, responseJson.location_suggestions[0].id))
+}
+
+function getZomatoRest(searchTerm, city_id, maxResults = 5) {
+    for (let i = 0; i < searchTerm.length; i++) {
+        const options = {
+            headers: new Headers({
+                'user-key': zomatoApiKey
+            })
+        };
+
+        const params = {
+            entity_id: city_id,
+            entity_type: 'city',
+            cuisines: getCuisineId(searchTerm[i]),
+            radius: 16095,
+            count: maxResults
+        };
+
+        let queryString = $.param(params);
+        const url = zomatoUrl + '/search?' +queryString;
+
+        fetch(url, options).then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error(response.statusText);
+            }).then(responseJson => displayRestResults(responseJson))
+            .catch(err => {
+                $('#js-error-message').text(`Something Failed ${err.message}`);
+            })
+    }
+}
+
+function displayRestResults(responseJson) {
+    for (let i = 0; i < responseJson.restaurants.length; i++) {
+        $('.rest-results').append(`<li>
+        <h3>${responseJson.restaurants[i].restaurant.name}</h3>
+        <a href='${responseJson.restaurants[i].restaurant.url}'>${responseJson.restaurants[i].restaurant.url}</a>
+        </li>`
+        )
+    };
+    $("#results").removeClass("hidden");
 }
 
 function selectCuisine() {
-  $('.cuisineSelection').on('click', '.cuisineOptions', function(event) {
-    $(event.target).toggleClass('active');
-  });
+    $('.cuisineSelection').on('click', '.cuisineOptions', function(event) {
+        $(event.target).toggleClass('active');
+    });
 }
 
 function whereToEat() {
-  $('.inOrOut').on('click', '.whereToEat', function(event) {
-    inOutFilter = $(event.target).text();
-    $(event.target).toggleClass('active');
-    console.log(inOutFilter);
-  });
+    $('.inOrOut').on('click', '.whereToEat', function(event) {
+        inOutFilter = $(event.target).text();
+        $(event.target).toggleClass('active');
+    });
 }
 
-function determineSearch(searchTerm, inOrOut, zip){
-  if (inOrOut == 'Cook it myself') {
-    //use only the youtube endpoint
-    getYoutubeVideos(searchTerm);
-  }
-  else if (inOrOut == 'Show all options') {
-    //use the zomato and youtube endpoint
-    getYoutubeVideos(searchTerm);
-    getZomatoRest(searchTerm,zip);
+function determineSearch(searchTerm, inOrOut, city_id) {
+    if (inOrOut == 'Home-cook') {
+        //use only the youtube endpoint
+        getYoutubeVideos(searchTerm);
+    } else if (inOrOut == 'Show all options') {
+        //use the zomato and youtube endpoint
+        getYoutubeVideos(searchTerm);
+        getZomatoRest(searchTerm, city_id);
 
-  }
-  else if (inOrOut == 'Dine out') {
-    //use only the zomato endpoint
-    getZomatoRest(searchTerm,zip);
-  };
+    } else if (inOrOut == 'Dine out') {
+        //use only the zomato endpoint
+        getCityId(searchTerm, city_id);
+    };
 }
 
-function gatherActive(){
-  $('div.cuisineOptions.active').each(function(){
-    selectionArr.push($(this).text());
-  });
+function gatherActive() {
+    $('div.cuisineOptions.active').each(function() {
+        selectionArr.push($(this).text());
+    });
 }
 
-function reset(){
-  $('.resetButton').on('click', function(){
-    $('.video-results').empty();
-    $('.rest-results').empty();
-    $("#results").addClass("hidden");
-    selectionArr = [];
-    inOutFilter = '';
-    $('.cuisineOptions').removeClass('active');
-    $('.whereToEat').removeClass('active');
-  })
+function reset() {
+    $('.resetButton').on('click', function() {
+        $('.video-results').empty();
+        $('.rest-results').empty();
+        $("#results").addClass("hidden");
+        selectionArr = [];
+        inOutFilter = '';
+        $('.cuisineOptions').removeClass('active');
+        $('.whereToEat').removeClass('active');
+    })
 }
 
-function watchForm(){
-  $('form').submit(event => {
-    event.preventDefault();
-    let zip = $('#zip').val();
-    gatherActive();
-    determineSearch(selectionArr, inOutFilter, zip);
-    console.log(selectionArr);
-  })
+function watchForm() {
+    $('form').submit(event => {
+        event.preventDefault();
+        let city = $('#city').val();
+        gatherActive();
+        determineSearch(selectionArr, inOutFilter, city);
+    })
 }
 
+$(carousel()); //hero carousel
 $(selectCuisine());
 $(whereToEat());
 $(watchForm());
